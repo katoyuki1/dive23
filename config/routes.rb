c0077460e079c7e129d31b01fda92bee9cd3c65b
YCHino <a2106349@gmail.com>
2016-11-02 19:34:37 +0900

    add edit route for answer resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'questions#index'
   resources :questions do
-  	resources :answer, only: [:create, :update, :destroy]
+  	resources :answer, only: [:create, :edit, :update, :destroy]
   end
   devise_for :users
 end
