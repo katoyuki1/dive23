1d5a3da0f8f437a673942715583b7e1fd3f40c7b
YCHino <a2106349@gmail.com>
2016-11-03 17:39:26 +0900

    add partial form for answer

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,17 @@ ActiveRecord::Schema.define(version: 20161102064532) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "answers", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "question_id"
+    t.text     "content",     null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "answers", ["question_id"], name: "index_answers_on_question_id", using: :btree
+  add_index "answers", ["user_id"], name: "index_answers_on_user_id", using: :btree
+
   create_table "questions", force: :cascade do |t|
     t.string   "title",      null: false
     t.text     "content",    null: false
@@ -45,4 +56,6 @@ ActiveRecord::Schema.define(version: 20161102064532) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "answers", "questions"
+  add_foreign_key "answers", "users"
 end
