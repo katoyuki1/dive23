34d91c81b24dcb1a9ab29bff4ece1070af3bc56a
YCHino <a2106349@gmail.com>
2016-11-03 19:34:41 +0900

    put the partials in question#show view and update answers/questions controllers

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,5 +1,15 @@
 class AnswersController < ApplicationController
   def create
+    @answer = current_user.answers.build(answer_params)
+    @question = @answer.question
+
+    respond_to do |format|
+      if @answer.save
+        format.html { redirect_to question_path(@question), notice: '回答を投稿しました。' }
+      else
+        format.html { render :new }
+      end
+    end
   end
 
   def edit
@@ -10,4 +20,9 @@ class AnswersController < ApplicationController
 
   def destroy
   end
+
+  private
+    def answer_params
+      params.require(:answer).permit(:question_id, :content)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'questions#index'
   resources :questions do
-  	resources :answer, only: [:create, :edit, :update, :destroy]
+  	resources :answers, only: [:create, :edit, :update, :destroy]
   end
   devise_for :users
 end
