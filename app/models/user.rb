ff16871d5ba31b43e16dbb503034292c67c66190
YCHino <a2106349@gmail.com>
2016-11-04 22:47:50 +0900

    add associations for users/questions to be associated through favorites

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
   has_many :questions, dependent: :destroy
   has_many :answers, dependent: :destroy
+  has_many :favorites
+  has_many :questions, through: :favorites
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
