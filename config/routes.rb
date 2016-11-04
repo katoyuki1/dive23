18dcd3c08ba37b75d228d5d59c4bfc873a40e8d5
YCHino <a2106349@gmail.com>
2016-11-04 22:56:08 +0900

    add routing for favorites

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root 'questions#index'
   resources :questions do
-  	resources :answer, only: [:create, :edit, :update, :destroy]
+  	resources :answers, only: [:create, :edit, :update, :destroy]
+  	resources :favorites, only: [:create, :destroy]
   end
   devise_for :users
 end
