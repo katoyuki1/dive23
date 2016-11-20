dad7ec35b6248a62c83218007c967e7f7f256c0c
YCHino <a2106349@gmail.com>
2016-11-20 14:12:25 +0900

    add vote counts in question#index

--- app/models/question.rb
+++ app/models/question.rb
@@ -5,4 +5,14 @@ class Question < ActiveRecord::Base
   has_many :users, through: :favorites
   has_many :votes, dependent: :destroy
   acts_as_taggable
+
+  def vote_question_counts
+    vote_plus = Vote.where(question_id: self.id, answer_id: nil, is_plus: true).count
+    vote_minus = Vote.where(question_id: self.id, answer_id: nil, is_plus: false).count
+    vote_count = vote_plus + (-1)*vote_minus
+    if vote_count.blank?
+      vote_count = 0
+    end
+    return vote_count
+  end
 end
