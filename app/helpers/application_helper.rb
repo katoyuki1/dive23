6bab8f879f7e67025535b2f7d98cb36db181c39b
YCHino <a2106349@gmail.com>
2016-11-23 11:50:32 +0900

    add a helper method to size users pics inusers#index

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,6 +8,16 @@ module ApplicationHelper
     image_tag(img_url, alt: user.name)
   end
 
+  # this helper is for users#index page
+  def user_profile_img(user)
+    if user.avatar?
+      return image_tag(user.avatar, alt: user.name, size: "60x60")
+    else
+      img_url = 'no_image.png'
+    end
+    image_tag(img_url, alt: user.name, size: "60x60")
+  end
+
   def question_count(user)
     count = Question.where(user_id: user.id).count
     if count > 0
