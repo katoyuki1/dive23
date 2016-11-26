ebf6e113fbdb1399f41c7e79d750ac4adf538615
YCHino <a2106349@gmail.com>
2016-11-26 15:48:52 +0900

    add rspec for answers controller

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -1,33 +1,54 @@
 require 'rails_helper'
 
-RSpec.describe AnswersController, type: :controller do
-
-  describe "GET #create" do
-    it "returns http success" do
-      get :create
-      expect(response).to have_http_status(:success)
+describe AnswersController do
+  describe 'Get #edit' do
+    before do
+      @question = create(:question)
+      @answer = create(:answer)
+      get :edit, {:question_id => @question.id, :id => @answer.id}
+    end
+    it 'リクエストは200 OKとなること' do
+      expect(response.status).to eq 200
+    end
+    it '@questionに要求されたquestionを割り当てること' do
+      expect(assigns(:answer)).to eq @answer
+    end
+    it ':editテンプレートを表示すること' do
+      expect(response).to render_template :edit
     end
   end
 
-  describe "GET #edit" do
-    it "returns http success" do
-      get :edit
-      expect(response).to have_http_status(:success)
+  describe 'Post #create' do
+    before do
+      @question = create(:question)
+      @answer = attributes_for(:answer)
+    end
+    it 'リクエストは302 リダイレクトとなること' do
+      post :create, question_id: @question.id, 
+        answer: {
+          question_id: @question.id,
+          content: "foo"
+        }
+      expect(response.status).to eq 302
+    end
+    it 'データベースに新しいユーザーが登録されること' do
+      expect {
+        post :create,
+        question_id: @question.id,
+        answer: {
+          question_id: @question.id,
+          content: "foo"
+        }
+      }.to change(Answer, :count).by(1)
+    end
+    it 'question_pathにリダイレクトすること' do
+      post :create,
+        question_id: @question.id,
+        answer: {
+          question_id: @question.id,
+          content: "foo"
+        }
+      expect(response).to redirect_to question_path(assigns(:question).id)
     end
   end
-
-  describe "GET #update" do
-    it "returns http success" do
-      get :update
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET #destroy" do
-    it "returns http success" do
-      get :destroy
-      expect(response).to have_http_status(:success)
-    end
-  end
-
 end
