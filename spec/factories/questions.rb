5644bb2acf8733f833126993cb1d0b1847c936b1
YCHino <a2106349@gmail.com>
2016-11-27 14:38:46 +0900

    update question factory file

--- spec/factories/questions.rb
+++ spec/factories/questions.rb
@@ -1,9 +1,4 @@
 FactoryGirl.define do
-  # factory :question do
-  #   sequence(:title) { |i| "title#{i}" }
-  #   sequence(:content) { |i| "content#{i}" }
-  # end
-
   factory :question do
     id 1
     title "foo"
